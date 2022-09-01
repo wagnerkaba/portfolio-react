ef30700342f6f65445dcbc9fe0a5e4c2a263465b
wagnerkaba <bom.link+github@gmail.com>
2022-09-01 13:05:39 -0300

    inclusão de link nos icones sociais

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,39 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { Link } from '@mui/material';
 
 function Footer() {
     return (
         <>
-            <Grid container display="flex" justifyContent="center" alignItems="center" sx={{ height: 60, backgroundColor: 'info.light' }}>
+            <Grid
+                container
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                sx={{ height: 60, backgroundColor: 'info.light' }}
+                spacing={2}
+            >
                 <Grid>
-                    <GitHubIcon fontSize={'large'} />
+                    <Link
+                        href="https://github.com/wagnerkaba"
+                        target="_blank"
+                        rel="noreferrer"
+                        color="inherit"
+                    >
+                        <GitHubIcon fontSize={'large'} />
+                    </Link>
                 </Grid>
                 <Grid>
-                    <LinkedInIcon fontSize={'large'}/>
+                    <Link
+                        href="https://www.linkedin.com/in/wagnerk-profile/"
+                        target="_blank"
+                        rel="noreferrer"
+                        color="inherit"
+                    >
+                        <LinkedInIcon fontSize={'large'} />
+                    </Link>
+
                 </Grid>
             </Grid>
         </>
