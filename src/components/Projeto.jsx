9a797e8d922a988941d5c2b8361dbf3c22bf5b48
wagnerkaba <bom.link+github@gmail.com>
2022-09-05 16:25:11 -0300

    adição do react-visibility-sensor

--- src/components/Projeto.jsx
+++ src/components/Projeto.jsx
@@ -1,49 +1,72 @@
+import * as React from 'react';
 import {
     Card,
     CardActions,
     CardContent,
     CardMedia,
     Button,
-    Typography
+    Typography,
+    Slide
 } from '@mui/material';
+import VisibilitySensor from 'react-visibility-sensor';
 
 
-const Projeto = ({titulo, imagem, descricao, linkDemo, linkGitHub }) => {
+
+const Projeto = ({ titulo, imagem, descricao, linkDemo, linkGitHub }) => {
+
+    let [active, setActive] = React.useState(false);
+
+
+
     return (
-        <Card>
-            <CardMedia
-                component="img"
-                height="300"
-                image={imagem}
-                alt={titulo}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    {titulo}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {descricao}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button
-                    size="small"
-                    href={linkDemo}
-                    target={"_blank"}
-                    rel={"noreferrer"}
-                >
-                    Demonstração
-                </Button>
-                <Button
-                    size="small"
-                    href={linkGitHub}
-                    target={"_blank"}
-                    rel={"noreferrer"}
-                >
-                    Github
-                </Button>
-            </CardActions>
-        </Card>
+        <VisibilitySensor
+            onChange={(partialVisibility) => {
+                if (partialVisibility) {
+                    setActive(true);
+                }
+            }}
+        >
+            <div>
+                <Slide direction='right' in={active} timeout={1000}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            height="300"
+                            image={imagem}
+                            alt={titulo}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {titulo}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {descricao}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Button
+                                size="small"
+                                href={linkDemo}
+                                target={"_blank"}
+                                rel={"noreferrer"}
+                            >
+                                Demonstração
+                            </Button>
+                            <Button
+                                size="small"
+                                href={linkGitHub}
+                                target={"_blank"}
+                                rel={"noreferrer"}
+                            >
+                                Github
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </Slide >
+            </div>
+        </VisibilitySensor >
+
+
     );
 
 }
--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -7,51 +7,69 @@ import {
 import Grid from '@mui/material/Unstable_Grid2';
 import Projeto from './Projeto';
 import { data } from '../data';
+import * as React from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 function Projetos() {
     const theme = useTheme();
     let bgColor = theme.palette.mode === 'dark' ? 'background.default' : 'info.light';
+    let [active, setActive] = React.useState(false);
 
 
     return (
-        <>
-            <Grid id="portfolio" sx={{ backgroundColor: bgColor}}>
 
-                <Grid xs={12} display="flex" justifyContent="center" alignItems="center" sx={{ mt:3, pt: 8, pb: 3 }}>
-                    <Typography variant="h2">
-                        <strong>Portfólio</strong>
-                    </Typography>
-                </Grid>
-                <Grid container xs={12} display="flex" justifyContent="center" alignItems="center">
+        <Grid id="portfolio" sx={{ backgroundColor: bgColor }}>
 
-                    {
-                        data.projetos.map((p) => {
-                            return (
-                                <Slide direction="up" in={true} key={p.titulo}>
-                                    <Grid xs={11} md={5} spacing={1} sx={{ p: 3 }}>
-                                        <Projeto
-                                            titulo={p.titulo}
-                                            imagem={p.imagem}
-                                            descricao={p.descricao}
-                                            linkDemo={p.linkDemo}
-                                            linkGitHub={p.linkGitHub}
-                                        />
-                                    </Grid>
-
-                                </Slide>
-
-                            );
+            <Grid xs={12} display="flex" justifyContent="center" alignItems="center" sx={{ mt: 3, pt: 8, pb: 3 }}>
+                <VisibilitySensor
+                    onChange={(isVisible) => {
+                        console.log(isVisible);
+                        if (isVisible) {
+                            setActive(true);
                         }
+                    }}
+                >
+                        <div>
+                            <Slide direction="left" in={active} timeout={1000}>
 
-                        )
+                                <Typography variant="h2">
+                                    <strong>Portfólio</strong>
+
+                                </Typography>
+
+                            </Slide>
+                        </div>
+                </VisibilitySensor>
+
+            </Grid>
+            <Grid container xs={12} display="flex" justifyContent="center" alignItems="center">
+
+                {
+                    data.projetos.map((p) => {
+                        return (
+                            <Grid xs={11} md={5} spacing={1} sx={{ p: 3 }} key={p.titulo}>
+                                <Projeto
+                                    titulo={p.titulo}
+                                    imagem={p.imagem}
+                                    descricao={p.descricao}
+                                    linkDemo={p.linkDemo}
+                                    linkGitHub={p.linkGitHub}
+                                />
+                            </Grid>
+
+
+                        );
                     }
 
+                    )
+                }
 
-                </Grid>
 
             </Grid>
 
-        </>
+        </Grid>
+
+
 
     );
 }
